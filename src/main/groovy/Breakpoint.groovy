ce5dc01a7fb206984e008971bfe0de9fbe271fb9
Simon Sadedin <ssadedin@gmail.com>
2017-03-27 10:00:12 +1100

    license as GPLv3

--- src/main/groovy/Breakpoint.groovy
+++ src/main/groovy/Breakpoint.groovy
@@ -1,3 +1,24 @@
+// vim: shiftwidth=4:ts=4:expandtab:cindent
+/////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of Schism.
+// 
+// Schism is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, under version 3 of the License, subject
+// to additional terms compatible with the GNU General Public License version 3,
+// specified in the LICENSE file that is part of the Schism distribution.
+//
+// Schism is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Schism.  If not, see <http://www.gnu.org/licenses/>.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 import java.sql.SQLException;
 
 import com.j256.ormlite.dao.Dao
